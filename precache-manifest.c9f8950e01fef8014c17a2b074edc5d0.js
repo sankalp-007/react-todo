fa2dd89346457a3f4cce70fa8983e6ff2a07b270
sankalp-007 <sankalp07652@gmail.com>
2019-12-30 23:57:34 +0530

    Updates

---
+++ precache-manifest.c9f8950e01fef8014c17a2b074edc5d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96726b3ba9779d9fe3c4ae1ad6ee3ea1",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "f6d55fd478211dbfb1a3",
+    "url": "/react-todo/static/js/2.e23471d4.chunk.js"
+  },
+  {
+    "revision": "83b3d04082a8c50f224f3e213e6a5a45",
+    "url": "/react-todo/static/js/2.e23471d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1ec7633c7313c719ae1",
+    "url": "/react-todo/static/js/main.575f56d1.chunk.js"
+  },
+  {
+    "revision": "0768ef87549d02f7e049",
+    "url": "/react-todo/static/js/runtime-main.0b801cd2.js"
+  }
+]);
